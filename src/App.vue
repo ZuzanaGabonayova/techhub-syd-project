475859ebf812a02ac6965d65142989d83651ba06
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:58:26 +0200

    updated app vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view/> -->
+  <div id="app">
+    <HomePage />
+    <AboutPage />
+    <ReferencesPage />
+  </div>
+
 </template>
 
+
+<script>
+import HomePage from './Vue/pages/HomePage.vue'
+import AboutPage from './Vue/pages/AboutPage.vue'
+import ReferencesPage from './Vue/pages/ReferencesPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    AboutPage,
+    ReferencesPage
+  }
+}
+
+</script>
+
+
+
 <style lang="scss">
-#app {
+
+@import "./style/main.css";
+
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +55,6 @@ nav {
       color: #42b983;
     }
   }
-}
+} */
+
 </style>
