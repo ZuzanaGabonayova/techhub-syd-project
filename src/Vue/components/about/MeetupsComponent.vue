1c8d49dd5a816de2e988616ae3afc4666244c9ab
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:54:51 +0200

    created co-founder meetups component

---
+++ src/Vue/components/about/MeetupsComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    
+    <section id="co-founder">
+
+        <div class="container-fluid meetups">
+
+            <div class="row co-founder-meetups">
+
+                <div class="col-sm-12 col-md-7 meetups-text">
+
+                    <h2>03</h2>
+                    <h3>Co-founder meetups</h3>
+                    <p>We organize a match-making event where commercial founders together with tech development founders can setup the right team from the start to create new tech ventures.
+                        <br>
+                        <br>It is often hard to find the right co-founder for your startup. 
+                    </p>
+
+                </div>
+
+                                
+                <div class="col-sm-12 col-md-5 meetups-img">
+                    <img src="../../../assets/images/illustrations/co-founder-meetups.gif" alt="co-founder-meetups">
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+         
+</template>
