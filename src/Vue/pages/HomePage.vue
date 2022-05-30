dccfd26a23300673e26c87c01a83c2a3da22ffab
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:51:49 +0200

    created homepage

---
+++ src/Vue/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div id="homeee">
+        <Navigation />
+        <Home />
+    </div>
+
+</template>
+
+
+<script>
+import Navigation from '../components/NavigationComponent.vue'
+import Home from '../components/home/HomeComp.vue'
+
+
+export default {
+    name: 'HomePage',
+
+    components:{
+        Navigation,
+        Home
+    }
+}
+</script>
+
+
+
+
