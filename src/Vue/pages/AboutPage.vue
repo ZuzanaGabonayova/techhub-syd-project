2338780617a06f0cdf1e891a1519c8b75930ea99
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:52:21 +0200

    created and updated about page

---
+++ src/Vue/pages/AboutPage.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div id="abouttt">
+        <Inspirational />
+        <Investors />
+        <Meetups />
+        <Startups />
+    </div>
+
+</template>
+
+
+<script>
+import Inspirational from '../components/about/InspirationalTalksComponent.vue'
+import Investors from '../components/about/InvestorPitchesComponent.vue'
+import Meetups from '../components/about/MeetupsComponent.vue'
+import Startups from '../components/about/StartupsComponent.vue'
+
+
+export default {
+    name: 'AboutPage',
+
+    components:{
+        Inspirational,
+        Investors,
+        Meetups,
+        Startups
+    }
+}
+</script>
+
+
+
+
