ebaadf40294fa07b46a00060ef594bf33c5415f5
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:55:03 +0200

    created investor pitches component

---
+++ src/Vue/components/about/InvestorPitchesComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    
+    <section id="investor">
+
+        <div class="container-fluid pitches">
+
+            <div class="row investor-pitches">
+
+                
+                <div class="col-sm-12 col-md-6 pitch-img">
+                    <img src="../../../assets/images/illustrations/investor-pitches.gif" alt="investor-pitches">
+                </div>
+
+
+                <div class="col-sm-12 col-md-6 pitch-text">
+
+                    <h2>02</h2>
+                    <h3>Investor pitches</h3>
+                    <p>We gather tech startups and smart investors to match the two so we can help fuel the next generation of tech startups through a powerful community.
+                        <br>
+                        <br>Startups will be chosen from four parameters such as: Team, Product/service, Market potential and business model.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+         
+</template>
