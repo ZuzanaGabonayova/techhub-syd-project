63577d23d9dd9c8e3c7816cc001500f51d8cc350
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:55:18 +0200

    created inspirational talks component

---
+++ src/Vue/components/about/InspirationalTalksComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    
+    <section id="inspirational">
+
+        <div class="container-fluid talks">
+
+            <div class="row insp-talks">
+
+                <div class="col-sm-12 col-md-7 insp-text">
+
+                    <h2>01</h2>
+                    <h3>Inspirational talks</h3>
+                    <p>We partner with inspirational leaders and tech entrepreneurs who have either built own venture or invested in startups.
+                        <br>
+                        <br>They go on stage to tell the true story of building tech companies, to inspire others and offer access to investors and students in the southern region.
+                    </p>
+
+                </div>
+
+
+                <div class="col-sm-12 col-md-5 insp-img">
+                    <img src="../../../assets/images/illustrations/inspirational-talks.gif" alt="inspirational-talks">
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+         
+</template>
