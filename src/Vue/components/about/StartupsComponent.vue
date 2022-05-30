b9049a4f496f04bacedeb42eddf089c2bd4c8c57
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:53:45 +0200

    created startups component

---
+++ src/Vue/components/about/StartupsComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    
+    <section id="startups">
+
+        <div class="container-fluid pitch">
+
+            <div class="row startups-pitch">
+
+                <div class="col-sm-12 col-md-6 startups-img">
+                    <img src="../../../assets/images/illustrations/startups.gif" alt="startups">
+                </div>
+
+
+                <div class="col-sm-12 col-md-6 startups-text">
+
+                    <h2>04</h2>
+                    <h3>Startups pitch for student jobs</h3>
+                    <p>We gather curious students from relevant educations and put them in front of startups looking to strengthen their core team. 
+                        <br>
+                        <br>Get inspired with inspirational talks from successful tech founders, industry leaders and business angels. It can be super exciting.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+         
+</template>
