dec239b801dc61554e9adc96a92366a81b10a547
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-05-31 00:51:39 +0200

    created references page

---
+++ src/Vue/pages/ReferencesPage.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div id="referencesss">
+        <References />
+    </div>
+
+</template>
+
+
+<script>
+import References from '../components/references/ReferencesComponent.vue'
+
+
+export default {
+    name: 'ReferencesPage',
+
+    components:{
+        References
+    }
+}
+</script>
+
+
+
+
