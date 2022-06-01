3941fa87faf18aa77f358e88a7ac6b6107841bb6
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-02 00:34:39 +0200

    create footer

---
+++ src/Vue/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <footer class="footer font-small text-center">
+    <div class="row">
+
+        <div class="col-sm-12 col-md-12 copyright">
+
+            <p>Copyright © {{ new Date().getFullYear() }} - TECHHUB SOUTH</p>
+
+            <p>By signing up to our community and newsletter you agree to receive updates on TECHHUB SOUTH events and news about relevant tech in the region.</p>
+        
+        </div>
+
+    </div>
+  </footer>
+
+</template>
+
