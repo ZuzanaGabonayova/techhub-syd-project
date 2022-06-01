8d4ea7ecce676212469fc19aed8e494a9057c0e1
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-01 10:39:31 +0200

    changed router

--- src/Vue/router/index.js
+++ src/Vue/router/index.js
@@ -53,20 +53,18 @@ export default router */
 
 
 
-
+/* 
 import { createRouter, createWebHistory } from 'vue-router'
 
 
-// for editing (CRUD)
+
 import Edit from '../pages/events/EditPage.vue'
 
-// For login stuff
+
 import Login from '../pages/events/LoginPage.vue'
 import Register from '../pages/events/RegisterPage.vue'
 
-// route guarding
-//import firebase from 'firebase'
-//import 'firebase/firestore'
+
 
 const routes = [
   {
@@ -75,7 +73,7 @@ const routes = [
     component: Login
   },
   {
-    path: '/edit/:id',  // id parameter to know which project we clicked
+    path: '/edit/:id',  
     name: 'Edit',
     component: Edit
   },
@@ -88,10 +86,61 @@ const routes = [
   {
     path: '/admin',
     name: 'Admin',
+
+    component: () => import('../pages/events/AdminPage.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+  
+})
+export default router */
+
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+// for editing (CRUD)
+import Edit from '../views/Edit.vue'
+
+// For login stuff
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+
+// route guarding
+//import firebase from 'firebase'
+//import 'firebase/firestore'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/edit/:id',  // id parameter to know which project we clicked
+    name: 'Edit',
+    component: Edit
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register', 
+    component: Register
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../pages/events/AdminPage.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue')
   }
 ]
 
@@ -101,4 +150,3 @@ const router = createRouter({
   
 })
 export default router
-
