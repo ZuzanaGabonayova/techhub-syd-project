baeaa994e98f9f30f45f00408bd3fb2542697206
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-01 09:49:39 +0200

    updated files

--- src/main.js
+++ src/main.js
@@ -19,3 +19,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app') 
+
+
+/* import { createApp } from 'vue'
+import App from './App.vue'
+import router from './Vue/router/index'
+
+createApp(App).use(router).mount('#app') */
