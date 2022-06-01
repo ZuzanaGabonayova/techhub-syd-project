0b2898e1ff97f7f33b7c195988892ac279f0845f
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-01 09:48:38 +0200

    updated contact page

---
+++ src/Vue/pages/ContactPage.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div id="contacttt">
+        <Idea />
+        <Join />
+    </div>
+
+</template>
+
+
+<script>
+import Idea from '../components/contact/IdeaComponent.vue'
+import Join from '../components/contact/JoinComponent.vue'
+
+
+
+export default {
+    name: 'AboutPage',
+
+    components:{
+        Idea,
+        Join
+    }
+}
+</script>
+
+
+
+
