25613d28d920587942e70096e128c44401b62de3
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-01 09:46:40 +0200

    create idea component

---
+++ src/Vue/components/contact/IdeaComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    
+    <section id="idea">
+
+        <div class="container-fluid idea">
+
+            <div class="row idea-heading">
+
+                <div class="col-sm-12 col-md-12 idea-heading-text">
+
+                    <h2>Got an idea you want to make real?</h2>
+
+                </div>
+
+            </div>
+
+            <div class="row idea-paragraph">
+
+                <div class="col-sm-12 col-md-12 idea-paragraph-text">
+
+                    <h3>This is your chance to meet with tech-focused investors looking for the most promising tech startups in the region.</h3>
+
+                </div>
+
+            </div>
+
+
+            <div class="row idea-connect">
+
+                <div class="col-md-12 idea-connect-img-line">
+                    <hr>
+                </div>
+
+                <div class="col-sm-12 col-md-4 idea-connect-img1">
+
+                    <img src="../../../assets/images/illustrations/1.png" alt="1st thingy">
+
+                </div>
+
+                <div class="col-sm-12 col-md-4 idea-connect-img2">
+
+                    <img src="../../../assets/images/illustrations/2.png" alt="2nd thingy">
+
+                </div>
+
+                <div class="col-sm-12 col-md-4 idea-connect-img3">
+
+                    <img src="../../../assets/images/illustrations/3.png" alt="3rd thingy">
+
+                </div>
+
+
+                <div class="col-sm-12 col-md-4 idea-connect-text1">
+                    
+                    <p>meet a solid group of investors and sharp VC&apos;s  ready to hear your pitch</p>
+
+                </div>
+
+                <div class="col-sm-12 col-md-4 idea-connect-text2">
+                    
+                    <p>be invited to do a 5 min. pitch</p>
+                    
+                </div>
+
+                <div class="col-sm-12 col-md-4 idea-connect-text3">
+                    
+                    <p>connect with investors and setup meetings to see if we have a match to scale your startup</p>
+                    
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </section>
+         
+</template>
+
