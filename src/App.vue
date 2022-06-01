843293e6518810eee5f96882b09267ba100e39dc
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-02 00:36:30 +0200

    update app

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,23 @@
   </nav>
   <router-view/> -->
   <div id="app">
+    <div class="description panel">
     <HomePage />
-    <AboutPage />
-    <ReferencesPage />
-    <ContactPage />
+    </div>
+    <section class="panel red">
+      <AboutPage />
+    </section>
+    <section class="panel orange">
+      <ReferencesPage />
+    </section>
+    <section class="panel purple">
+      <ContactPage />
+    </section>
+    <section class="panel green">
+      <!-- <EventsPage /> -->
+    </section>
 
-    <EventsPage />
+    <!-- <EventsPage /> -->
   </div>
 
 </template>
@@ -20,7 +31,7 @@
 import HomePage from './Vue/pages/HomePage.vue'
 import AboutPage from './Vue/pages/AboutPage.vue'
 import ReferencesPage from './Vue/pages/ReferencesPage.vue'
-import EventsPage from './Vue/pages/EventsPage.vue'
+/* import EventsPage from './Vue/pages/EventsPage.vue' */
 import ContactPage from './Vue/pages/ContactPage.vue'
 
 export default {
@@ -30,12 +41,31 @@ export default {
     AboutPage,
     ReferencesPage,
 
-    EventsPage,
+    /* EventsPage, */
     ContactPage
 
     /* EventsPage */
   }
 }
+
+/* import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.utils.toArray(".panel").forEach((panel) => {
+        ScrollTrigger.create({
+          trigger: panel,
+          start: "top top", 
+          pin: true, 
+          pinSpacing: false 
+        });
+      });
+
+
+      ScrollTrigger.create({
+        snap: 1 / 7 // snap whole page to the closest section!
+}); */
 
 </script>
 
