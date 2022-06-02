589745de724acb1f80bae62a8e903c0a7e0edec2
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-02 07:42:37 +0200

    changes in style

--- src/main.js
+++ src/main.js
@@ -14,15 +14,15 @@ new Vue ({
 }).$mount('#app') */
 
 
-
+/* 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app') 
+createApp(App).mount('#app')  */
 
 
-/* import { createApp } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Vue/router/index'
 
-createApp(App).use(router).mount('#app') */
+createApp(App).use(router).mount('#app')
