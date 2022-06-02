e385cf1a1baf5642b80d0ed6426a785cd1c7f9f6
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2022-06-02 14:31:43 +0200

    create home view

---
+++ src/Vue/pages/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+        
+    <NavigatonComponent />
+    <HomePage />
+    <AboutPage />
+    <EventsPage />
+    <ReferencesPage />
+    <ContactPage />
+
+</template>
+
+<script>
+import NavigatonComponent from '../components/NavigationComponent.vue'
+import HomePage from './HomePage.vue'
+import AboutPage from './AboutPage.vue'
+import ReferencesPage from './ReferencesPage.vue'
+import EventsPage from './EventsPage.vue'
+import ContactPage from './ContactPage.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    NavigatonComponent,
+    HomePage,
+    AboutPage,
+    EventsPage,
+    ReferencesPage,
+    ContactPage
+  }
+}
+</script>
